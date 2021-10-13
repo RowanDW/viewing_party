93ceac9393093a974ce565791bcb3f1ec220a45f
Rowan DeLong <rowanwinzer@gmail.com>
2021-10-13 15:46:18 -0600

    Fix rubocop styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,11 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :invites, foreign_key: :guest_id
   # has_many :parties, foreign_key: :host_id, class_name: "Party"
-  has_many :hosted_parties, foreign_key: :host_id, class_name: "Party"
-  has_many :parties, foreign_key: :guest_id, class_name: "Party", through: :invites
-  #XX has_many :guest_parties, foreign_key: :guest_id, class_name: "Party", through: :invites
+  has_many :hosted_parties, foreign_key: :host_id, class_name: 'Party'
+  has_many :parties, foreign_key: :guest_id, class_name: 'Party', through: :invites
+  # XX has_many :guest_parties, foreign_key: :guest_id, class_name: "Party", through: :invites
 
-  has_many :friends, foreign_key: :friend_id, class_name: "User", through: :friendships #?
-
+  has_many :friends, foreign_key: :friend_id, class_name: 'User', through: :friendships # ?
 end
 
 # has_many :parties, through: :invites
