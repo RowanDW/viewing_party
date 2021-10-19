7d80b062df99fc038ef8204b769f0bfcab8ac0e5
Rowan DeLong <rowanwinzer@gmail.com>
2021-10-19 16:10:26 -0600

    Add title method for party

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,13 +1,15 @@
 class Party < ApplicationRecord
   validates :movie, :duration, :day, :start_time, presence: true
-  validates :host_id, presence: true  # #?
+  validates :host_id, presence: true
   validates :duration, numericality: true
 
   has_many :invites, dependent: :destroy
-  has_many :guests, foreign_key: :guest_id, through: :invites # ?
+  has_many :guests, foreign_key: :guest_id, through: :invites
 
-  belongs_to :host, class_name: 'User' # ?
+  belongs_to :host, class_name: 'User'
 
-  # has_many :users, through: :invites  #AS guests
-  # belongs_to :user  #AS host
+  def title
+    movie = MovieFacade.movie_show(self.movie)
+    movie.title
+  end
 end
