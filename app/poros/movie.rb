c40eb5d2e0aab3e8988e6ec2ad63c6affa07ca0f
kevn7n <kevn_nguyen@yahoo.com>
2021-10-19 10:05:41 -0600

    Update new party

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -6,42 +6,36 @@ class Movie
               :genres,
               :overview,
               :cast,
-              :reviews,
-              :poster
-
+              :reviews
 
   def initialize(main_info, cast_info, rev_info)
     @id             = main_info[:id]
     @title          = main_info[:title]
     @vote_average   = main_info[:vote_average]
     @runtime        = main_info[:runtime]
-    @genres         = format_genres(main_info[:genres])
+    @genres         = list_genres(main_info[:genres])
     @overview       = main_info[:overview]
-    @cast           = format_cast(cast_info[:cast])
-    @reviews        = format_reviews(rev_info[:results])
-    # @poster         = add_base_url(main_info[:poster_path])
+    @cast           = list_cast(cast_info[:cast])
+    @reviews        = list_reviews(rev_info[:results])
   end
 
-  def format_genres(data)
+  def list_genres(data)
     genres = data.map do |genre|
       genre[:name]
     end
   end
 
-  def format_cast(data)
+  def list_cast(data)
     cast = data.map do |actor|
       {actor: actor[:name], character: actor[:character]}
     end
     cast = cast.slice(0, 10)
   end
 
-  def format_reviews(data)
+  def list_reviews(data)
     reviews = data.map do |rev|
       {author: rev[:author], content: rev[:content]}
     end
   end
 
-  # def add_base_url(path)
-  #   "https://image.tmdb.org/t/p/w342/" + path
-  # end
 end
