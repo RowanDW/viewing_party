19e904664168c03a1da5f78a6c7737fd592f6a71
kevn7n <kevn_nguyen@yahoo.com>
2021-10-20 16:00:26 -0600

    Complete new party and spec test

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,29 +1,34 @@
 class PartiesController < ApplicationController
   def new
     @movie = MovieFacade.movie_show(params[:id])
-    @party = Party.new(duration: @movie.runtime)
+    @party = Party.new
     @friends = current_user.friends
   end
 
   def create
-    @movie = MovieFacade.movie_show(params[:movie_id])
-    @friends = current_user.friends
-    
-    party[:host_id] = current_user.id
+    @movie = MovieFacade.movie_show(party_params[:movie])
     # require "pry"; binding.pry
-    if party[:duration].to_i > @movie.runtime
-      new_party = Party.create(party)
+    if party_params[:duration].to_i >= @movie.runtime
+      new_party = Party.new(party_params)
+      # require "pry"; binding.pry
+
       if new_party.save
-        # add_party_guest(new_party) if params[:friend]
+        if params[:friend_add]
+          params[:friend_add].each do |friend, invite|
+            if invite == "1"
+              Invite.create!(party_id: new_party.id, guest_id: friend.to_i)
+            end
+          end
+        end
         redirect_to dashboard_path
         flash[:success] = "You've created a Viewing Party!"
       else
-        render :new
-        flash[:error] = "Please enter a valid start time, date and duration."
+        redirect_to "/parties/new?id=#{party_params[:movie]}"
+        flash[:error] = "You must enter a valid start time, date and duration."
       end
     else
-      render :new
-      flash[:error] = "Party duration must be more than movie run time."
+      redirect_to "/parties/new?id=#{party_params[:movie]}"
+      flash[:error] = "Party duration cannot be less than movie run time."
     end
   end
 
@@ -32,6 +37,6 @@ class PartiesController < ApplicationController
   #   end
 
   def party_params
-    params.permit(:movie_id, :duration, :start_time, :day, :title)
+    params.require(:party).permit(:host_id, :duration, :start_time, :day, :movie)
   end
 end
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -7,38 +7,41 @@ RSpec.describe "Parties New form" do
     @jules = User.create(name: 'Jules', email: "jules@test.com", password: "test")
     @hanna = User.create(name: 'Hanna', email: "hanna@test.com", password: "test")
 
-    # @movie = Movie.create!(id: 550988, title: "Free Guy", runtime: 115)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@kevin)
   end
 
   context "if user has no added friends" do
-    xit "creates a party" do
+    it "creates a party", :vcr do
 
-      visit "/parties/new?id=550988"
-      save_and_open_page
-      fill_in 'date', with: '12/12/21'
-      fill_in 'start_time', with: '5:00 PM'
-      fill_in 'duration', with: 200
+      visit "/movies/550988"
+      click_on "New Viewing Party"
+
+      fill_in 'party[day]', with: '12/12/21'
+      fill_in 'party[start_time]', with: '5:00 PM'
+      fill_in 'party[duration]', with: 200
       click_on "Create Party"
 
-      expect(page).to_not have_content("Invite:")
       expect(page).to have_content("You've created a Viewing Party!")
       expect(current_path).to eq(dashboard_path)
     end
 
-    xit "can't create party with a duration less than the movie runtime" do
+    it "can't create party with a duration less than the movie runtime", :vcr do
 
-      fill_in 'date', with: '12/12/21'
-      fill_in 'start_time', with: '5:00 PM'
-      fill_in 'duration', with: 55
+      visit "/movies/550988"
+      click_on "New Viewing Party"
+
+      fill_in 'party[day]', with: '12/12/21'
+      fill_in 'party[start_time]', with: '5:00 PM'
+      fill_in 'party[duration]', with: 2
       click_on "Create Party"
 
       expect(page).to have_content("Party duration cannot be less than movie run time.")
     end
 
-    xit "can't create a party with empty fields" do
+    it "can't create a party with empty fields", :vcr do
+      visit "/movies/550988"
+      click_on "New Viewing Party"
 
-      fill_in 'duration', with: 190
       click_on "Create Party"
 
       expect(page).to have_content("You must enter a valid start time, date and duration.")
@@ -46,25 +49,33 @@ RSpec.describe "Parties New form" do
   end
 
 
-  # context "if user has friends" do
-  #   it 'shows friends you can invite on the form' do
-  #     @kevin.friends.push(@rowan, @jules)
+  context "if user has friends" do
+    it 'shows friends you can invite on the form', :vcr do
+      @kevin.friends.push(@rowan, @jules)
 
-  #     expect(page).to have_content("Invite:")
-  #     expect(page).to have_content(@rowan.name)
-  #     expect(page).to have_content(@jules.name)
-  #   end
-  #
-  #   it "creates party with invited friends" do
-  #
-  #     fill_in 'date', with: '12/12/21'
-  #     fill_in 'start_time', with: '5:00 PM'
-  #     fill_in 'duration', with: 200
-  #     check "#{@rowan.name}]"
-  #     click_on "Create Party"
-  #
-  #     expect(current_path).to eq(dashboard_path)
-  #     # expect(page).to have_content("Free Guy")
-  #   end
-  # end
+      visit "/movies/550988"
+      click_on "New Viewing Party"
+
+      expect(page).to have_content(@rowan.name)
+      expect(page).to have_content(@jules.name)
+    end
+
+    it "creates party with invited friends", :vcr do
+      @kevin.friends.push(@rowan, @jules)
+
+      visit "/movies/550988"
+      click_on "New Viewing Party"
+
+      fill_in 'party[day]', with: '12/12/21'
+      fill_in 'party[start_time]', with: '5:00 PM'
+      fill_in 'party[duration]', with: 200
+      check "friend_add[#{@rowan.id}]"
+      click_on "Create Party"
+
+      expect(page).to have_content("You've created a Viewing Party!")
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Free Guy")
+      expect(page).to have_content("Rowan")
+    end
+  end
 end
